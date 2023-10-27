2a3d7dc844e8d44d07975d704cabb9447c9e9fae
kathariya Arun <kathariya12@gmail.com>
2023-10-27 19:30:36 +0900

    files updated

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 let nav=document.querySelector(".navbar")
 window.onscroll=function(){
 
-    if(document.documentElement.scrollTop > 20) {
+    if(document.documentElement.scrollTop >20) {
         nav.classList.add("header-scrolled")
     }
     else{
